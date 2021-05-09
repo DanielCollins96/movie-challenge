3f951e8ac2db0a96cbacd0aba5abaf2e0e46599b
dc <danielmcollins1996@gmail.com>
2021-05-09 14:27:03 -0600

    style

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: theme => ({
+       'body-bg': "url('/felix-mooneeram-evlkOfkQ5rE-unsplash.jpg')",
+      })
+    },
   },
   variants: {
     extend: {},
